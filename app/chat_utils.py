458908c354109b4b5b60503fa85d667999c55582
SteveKevinDias <diassteve962@gmail.com>
2026-02-03 20:52:49 +0530

    changes made

--- app/chat_utils.py
+++ app/chat_utils.py
@@ -1,16 +1,17 @@
 from euriai.langchain import create_chat_model
 
-API_KEY = None
 MODEL = "gpt-4.1-nano"
 TEMPERATURE = 0.7
 
-def get_chat_model(api_key: str = None):
+
+def get_chat_model(api_key: str):
     return create_chat_model(
-        api_key=api_key or API_KEY,
+        api_key=api_key,
         model=MODEL,
         temperature=TEMPERATURE
     )
 
+
 def ask_chat_model(chat_model, prompt: str):
     response = chat_model.invoke(prompt)
-    return response.content 
+    return response.content
